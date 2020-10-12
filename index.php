da8af72fa306871aa07c45885edab8daf5adca43
BoldizArt <boldizar.santo@gmail.com>
2020-10-12 19:16:50 +0200

    Create a simple router

--- index.php
+++ index.php
@@ -6,7 +6,35 @@ error_reporting(E_ALL);
 
 require_once(__DIR__.'/vendor/autoload.php');
 
-use Boldizar\LibraFire\Controller\Controller;
+// This is a simple route function
+// Not a good solution, but for this case ok
+// It's working
+$request = $_SERVER['REQUEST_URI'];
 
-$controller = new Controller();
-$controller->test();
+switch ($request) {
+    case '/' :
+        echo 'Home page';
+        break;
+    case '/students' :
+    case '/students/' :
+        echo 'Students page';
+        break;
+    default:
+        // Check for students request with an id
+        if (substr($request, 0, 10) == '/students/') {
+            // Explode the request string and create an array
+            $params = array_filter(explode('/', $request));
+            $params = array_values($params);
+
+            // Check is there an valid id in the request
+            if (isset($params[1]) && is_numeric($params[1]) && !isset($params[2])) {
+                $id = (int) $params[1];
+                echo "Fetch the student with id: <b>{$id}</b>";
+                break;
+            }
+        }
+
+        http_response_code(404);
+        echo 'Page not found';
+        break;
+}
